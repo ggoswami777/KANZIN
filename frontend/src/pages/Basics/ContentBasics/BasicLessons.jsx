e1a5c03b3c57a7fc5da3978d34592d2fb01fc6aa
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-11-25 21:08:00 +0530

    added lessons data for the basics

---
+++ frontend/src/pages/Basics/ContentBasics/BasicLessons.jsx
@@ -0,0 +1,20 @@
+export const BasicsLessons = [
+  {
+    lesson: 1,
+    topic: "Hiragana",
+    youtube: "https://youtu.be/6p9Il_j0zjc?si=lXdkg3-tahfRd5Nx",
+    pdf: "https://www.szfki.hu/~akiss/others/hiragana_table.pdf"
+  },
+  {
+    lesson: 2,
+    topic: "Katakana",
+    youtube: "https://youtu.be/s6DKRgtVLGA?si=FpjX6_CvwvULv3gY",
+    pdf: "https://www.szfki.hu/~akiss/others/katakana_table.pdf"
+  },
+  {
+    lesson: 3,
+    topic: "Basic Phrases",
+    youtube: "https://youtu.be/4Q22hN-1mLI?si=5Il-usgyk0zfKOIA",
+    pdf: null
+  }
+];
